d5d0c8387105a6608e5b7c37c1b3da5450931097
João Pedro <joaopedrodeveloper@outlook.com>
2025-04-23 19:00:59 -0300

    "Criei o Desafio basíco de Java"

---
+++ Java/Java-Basico/DesafioBasico/Main.java
@@ -0,0 +1,25 @@
+package DesafioBasico;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //Instanciando o Objeto que vai ler nossos Dados
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite seu Nome: ");
+        String nome = input.nextLine();
+
+        System.out.print("Digite o Ano que você nasceu: ");
+        int ano = input.nextInt();
+
+        if ((ano < 1920) || (ano > 2026)) {
+            System.out.println("Digite uma data valida!");
+        }
+        else {
+            int idade = 2025 - ano;
+            System.out.printf("Olá %s você tem %d anos!",nome,idade);
+        }
+
+    }
+}
